7285d67679a2b6b9338eff88748ad66bb4e90dde
Epicode <40544619+Epicoded@users.noreply.github.com>
2022-01-14 16:28:03 -0600

    Adding new variables

--- database/Conector.java
+++ database/Conector.java
@@ -8,27 +8,25 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Conector {
-	
-	static String url = "C:/Users/MXI02003132A/Desktop/UnaDM/Base/mensajes.db";
+	final static String DIRECTORY = System.getProperty("user.dir");
+
+	static final String URL = DIRECTORY + "/data/mensajes.db";
 	static Connection connect;
-	
-	public static Connection connect(){
-		 try {
-		     connect = DriverManager.getConnection("jdbc:sqlite:"+url);
-//		     if (connect!=null) {
-//		         System.out.println("Conectado");
-//		     }
-		 }catch (SQLException ex) {
-		     System.err.println("No se ha podido conectar a la base de datos\n"+ex.getMessage());
-		 }
-		 return connect;
+
+	public static Connection connect() {
+		try {
+			connect = DriverManager.getConnection("jdbc:sqlite:" + URL);
+		} catch (SQLException ex) {
+			System.err.println("No se ha podido conectar a la base de datos\n" + ex.getMessage());
 		}
-	
-		 public void close(){
-		        try {
-		            connect.close();
-		        } catch (SQLException ex) {
-		            Logger.getLogger(Conector.class.getName()).log(Level.SEVERE, null, ex);
-		        }
-		 }
+		return connect;
+	}
+
+	public void close() {
+		try {
+			connect.close();
+		} catch (SQLException ex) {
+			Logger.getLogger(Conector.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
